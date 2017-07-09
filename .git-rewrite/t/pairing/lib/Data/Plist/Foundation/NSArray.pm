fee16c321670c917f3905a276ec1a31a923d72a4
philippe44 <philippe_44@outlook.com>
2017-07-09 11:37:20 -0700

    cleanup

--- .git-rewrite/t/pairing/lib/Data/Plist/Foundation/NSArray.pm
+++
@@ -1,9 +0,0 @@
-package Data::Plist::Foundation::NSArray;
-
-use strict;
-use warnings;
-
-use base qw/Data::Plist::Foundation::NSObject/;
-
-1;
-
--- .git-rewrite/t/pairing/lib/Data/Plist/Foundation/ToDo.pm
+++
@@ -1,9 +0,0 @@
-package Data::Plist::Foundation::ToDo;
-
-use strict;
-use warnings;
-
-use base qw/Data::Plist::Foundation::NSObject/;
-
-1;
-
