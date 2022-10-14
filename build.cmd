7979cd81731e4c7737fd99c1613fb6ad18bdef9f
philippe44 <philippe44@users.noreply.github.com>
2022-10-13 18:48:57 -0700

    add build  script for Windows

---
+++ build.cmd
@@ -0,0 +1,23 @@
+setlocal
+
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
+
+if /I [%1] == [rebuild] (
+	set option="-t:Rebuild"
+)
+
+rem msbuild libraoptools.sln /property:Configuration=Debug %option%
+msbuild raoptools.sln /property:Configuration=Release %option%
+
+set target=targets\win32\x86
+
+if exist %target% (
+	del %target%\*.lib
+)
+
+robocopy lib\win32\x86 %target% lib*.lib lib*.pdb /NDL /NJH /NJS /nc /ns /np
+robocopy src targets\include raop_client.h /NDL /NJH /NJS /nc /ns /np
+robocopy src targets\include raop_server.h /NDL /NJH /NJS /nc /ns /np
+
+endlocal
+
