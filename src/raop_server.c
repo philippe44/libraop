1cc797dfb2dcbde02ae7786ec0df54d510ade260
philippe44 <philippe44@users.noreply.github.com>
2023-04-17 23:29:17 +0200

    use .jpg

--- src/raop_server.c
+++ src/raop_server.c
@@ -573,7 +573,7 @@ static bool handle_rtsp(raopsr_t *ctx, int sock)
 		} else if (body && ((p = kd_lookup(headers, "Content-Type")) != NULL) && strcasestr(p, "image/jpeg")) {
 				LOG_INFO("[%p]: received JPEG image of %d bytes", ctx, len);
 				char buffer[16];
-				sprintf(buffer, "/%x", ctx->metadata.title ? hash32(ctx->metadata.title) + (unsigned)&body : (unsigned)&body);
+				sprintf(buffer, "/%x.jpg", ctx->metadata.title ? hash32(ctx->metadata.title) + (unsigned)&body : (unsigned)&body);
 				NFREE(ctx->metadata.artwork);
 				ctx->metadata.artwork = http_pico_add_source(buffer, "image/jpeg", body, len, 120);
 				ctx->flushedArtwork = false;
