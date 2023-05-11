3b8850dc658e17e80b6aa9fb34e6f4a4a4b1181e
philippe44 <philippe44@users.noreply.github.com>
2023-05-11 13:38:45 +0200

    just use static option

--- build.cmd
+++ build.cmd
@@ -7,7 +7,7 @@ if /I [%1] == [rebuild] (
 )
 
 rem msbuild libraop.sln /property:Configuration=Debug %option%
-msbuild libraop.sln /property:Configuration=Static %option%
+msbuild libraop.sln /p:Configuration=Release /p:Platform=x86 %option%
 
 set target=targets\win32\x86
 
